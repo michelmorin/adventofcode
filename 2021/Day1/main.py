1d2a8907c7bf2d16838e934a5db2090ab740bf48
michelmorin <michelmorin77@gmail.com>
2022-01-22 15:24:57 -0500

    Commit Day 1 in 2021

--- 2021/Day1/main.py
+++ 2021/Day1/main.py
@@ -3,28 +3,24 @@ def has_it_increased(previous, current):
 
 
 with open("input.txt", "r") as file:
-    depths = file.readlines()
+    depths = file.read().splitlines()
     count_increased = 0
 
     for index, depth in enumerate(depths):
         if index >= 1:
-            if has_it_increased(int(depths[index-1].rstrip()), int(depths[index].rstrip())):
+            if has_it_increased(int(depths[index-1]), int(depths[index])):
                 count_increased += 1
 
     print(f"Part 1: The number depths increased is: {count_increased}")
 
 with open("input.txt", "r") as file:
-    depths = file.readlines()
+    depths = file.read().splitlines()
     count_increased = 0
 
     for index, depth in enumerate(depths):
         if index >= 3:
-            previous_sum = int(depths[index - 3].rstrip()) \
-                + int(depths[index - 2].rstrip()) \
-                + int(depths[index - 1].rstrip())
-            current_sum = int(depths[index - 2].rstrip()) \
-                + int(depths[index - 1].rstrip()) \
-                + int(depths[index].rstrip())
+            previous_sum = sum([int(d) for d in depths[index - 3:index]])
+            current_sum = sum([int(d) for d in depths[index - 2:index+1]])
 
             if has_it_increased(previous_sum, current_sum):
                 count_increased += 1
